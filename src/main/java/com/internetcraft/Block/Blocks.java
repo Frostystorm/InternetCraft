e087679012e4fe6394ad04f753693286c9a6f938
Sinan <sinaneonder@gmail.com>
2015-05-30 20:36:41 -0400

    Added Bacon Block w/ textures and models

--- src/main/java/com/internetcraft/Block/Blocks.java
+++ src/main/java/com/internetcraft/Block/Blocks.java
@@ -8,18 +8,24 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class Blocks
 {
-	public final static Block doge = new BlockInternetCraft(Material.ground)
+	public final static Block doge_block = new BlockInternetCraft(Material.ground)
 			.setHardness(0.5F).setStepSound(Block.soundTypeMetal)
 			.setUnlocalizedName("doge_block");
+	
+	public final static Block bacon_block = new BlockInternetCraft(Material.ground)
+	.setHardness(0.5F).setStepSound(Block.soundTypeMetal)
+	.setUnlocalizedName("bacon_block");
 
 	public static void init()
 	{
-		GameRegistry.registerBlock(doge, "doge_block");
-
+		GameRegistry.registerBlock(doge_block, "doge_block");
+		GameRegistry.registerBlock(bacon_block, "bacon_block");
 	}
 
 	public static void registerModels()
 	{
 		Items.registerRenderer(GameRegistry.findItem(Reference.MODID, "doge_block"), "doge_block");
+		Items.registerRenderer(GameRegistry.findItem(Reference.MODID, "bacon_block"), "bacon_block");
+
 	}
 }
